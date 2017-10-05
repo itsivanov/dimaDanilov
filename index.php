edd84c3557cba8dd669402f85994fda4a5bba71b
Ivanov <o.ivanov@zoodoc.de>
2017-10-05 23:10:34 +0300

    rm

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once 'model.php';
+require_once 'config.php';
 
 
 $query = "SELECT * FROM city";
--- model.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: oleg
- * Date: 05.10.17
- * Time: 21:05
- */
-
-include_once 'config.php';
