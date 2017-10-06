b1548ec2010c9b39b748f95a23ccc732c9b1f4cd
Ivanov <o.ivanov@zoodoc.de>
2017-10-06 05:30:11 +0300

    rm

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
-<?php
-
-require_once 'config.php';
-
-
-$query = "SELECT * FROM city";
-$stmt = $GLOBALS['DB']->prepare( $query );
-$stmt->execute();
-$object =  $stmt->fetchAll( PDO::FETCH_ASSOC );
-
-?>
 <script type="text/javascript" src="/assets/js/jquery-3.2.1.min.js"></script>
 <script type="text/javascript" src="/assets/js/main.js"></script>
 
